98d879140c53ff8b4f9ae228675f9d0ee466fa6c
saiduthv <49785399+saiduthv@users.noreply.github.com>
2020-04-10 21:40:25 +0530

    Add files via upload

---
+++ zenfone.php
@@ -0,0 +1,222 @@
+<?php
+include "navbarsbootsdtrap.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Zenfone</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  	.img1
+  	{
+  		width: 100%;
+  		height: 100%;
+  		border-radius: 20px;
+  		transition: transform .2s;
+  		padding: 10px;
+  	}
+  	.img2
+  	{
+  		font-style: italic;
+  	}
+  	.img1:hover
+  	{
+  		opacity: 0.75;
+  		transition-duration: 0.2s;
+  		transform: scale(1.1);
+  	}
+  	.footer 
+  	{
+   position: sticky;
+   left: 0;
+   bottom: 0;
+   background-color:rgb(0, 26, 51);
+   color: white;
+   border-radius: 15px;
+   padding: 15px;
+   height: 70px;
+   font-size: 30px;
+
+}
+.bod
+{
+	background-color: black;
+  		color: white;
+}
+.ta
+{
+	padding-left: 20px;
+}
+.car
+{
+	border-radius: 20px;
+}
+.c
+{
+	font-size: 15px;
+}
+.ka
+{
+	background-color: rgb(217, 218, 195);
+	border-radius: 50px;
+}
+tr,th,td
+{
+	border: 2px solid white;
+	padding:10px;
+}
+</style>
+</head>
+<body class="bod">
+<div class="container ">
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-4">
+  <div id="myCarousel" class="carousel slide " data-ride="carousel">
+    <ol class="carousel-indicators ">
+     <li data-target="#myCarousel" data-slide to="0" class="active "></li>	
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner ">
+
+      <div class="item active ">
+        <img src="asus-zenfone-5z-zs621kl-2a001in-zs621kl-2a011in-original-imaf6rn7z9ywgkpz.jpeg" alt="Vivo U10" style="width: 100%; height: auto;">
+      </div>
+
+      <div class="item">
+        <img src="asus-zenfone-5z-zs621kl-2a001in-zs621kl-2a011in-original-imaf6rn7eyfngknj.jpeg" alt="Vivo U10" style="width: 100%; height: auto;">
+      </div>
+    
+      <div class="item">
+        <img src="asus-zenfone-5z-zs621kl-2a001in-zs621kl-2a011in-original-imaf6rn7pczcdngz.jpeg" alt="Vivo U10" style="width: auto; height: 720px;">
+      </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
+<div class="col-md-1"></div>
+<div class="col-md-6">
+	<div>
+		<p>Price:<s style="color: red;">32,999</s></p>
+		<h3>16,999/-</h3>
+	</div>
+	<table>
+		<tr>
+			<th>components</th>
+			<th>Dimensions</th>
+		</tr>
+		<tr>
+			<td>Width</td>
+			<td>75.65 mm</td>
+		</tr>
+		<tr>
+			<td>Height</td>
+			<td>153 mm</td>
+		</tr>
+		<tr>
+			<td>Weight</td>
+			<td>165 gms</td>
+		</tr>
+		<tr>
+			<td>Depth</td>
+			<td>7.7 mm</td>
+		</tr>
+		<tr>
+			<td>Model No: </td>
+			<td>ZS621KL-2A001IN / ZS621KL-2A011IN</td>
+		</tr>
+		<td>Browse Type</td>
+		<td>Smartphones</td>
+		<tr>
+		<td>Sim Type</td>
+		<td>Dual Sim</td>
+	</tr>
+	<tr>
+		<td>Internal storage</td>
+		<td>64GB</td>
+	</tr>
+	<tr>
+		<td>RAM</td>
+		<td>4GB</td>
+	</tr>
+	<tr>
+		<td>Operating System</td>
+		<td>Android oreo 8</td>
+	</tr>
+	<tr>
+		<td>Camera</td>
+		<td>12MP + 8MP</td>
+	</tr>
+	<tr>
+		<td>Flash</td>
+		<td>Rear Flash</td>
+	</tr>
+	<tr>
+		<td>Supported Networks</td>
+		<td>GSM, WCDMA, 4G VoLTE, UMTS, 4G LTE</td>
+	</tr>
+	<tr>
+		<td>Battery</td>
+		<td>3300 mAh</td>
+	</tr>
+	<tr>
+		<td>Memory card slot</td>
+		<td>dedicated Slot</td>
+	</tr>
+	<tr>
+		<td>Audio Jack</td>
+		<td>3.5 mm</td>
+	</tr>
+	</table>
+</div>
+</div>
+<div class="row">
+	<div class="col">
+		<h3>Product Details</h3>
+		<h4>Performance Redefined</h4>
+		<p>It features a 2.8-GHz octa-core Snapdragon 845 processor with the Artifical Intelligence Engine (AIE). With the Adreno 630 processor and 6 GB of RAM, any apps and games that you may run work seamlessly.</p>
+		<br>
+		<h4>Picture-perfect Memories, Day or Night</h4>
+		<p>Capture your precious moments with the 12 MP camera which features a Sony IMX363 flagship sensor, f/1.8 aperture, and a pixel size of 1.4 micrometres. It also comes with other features such as, Optical Image Stabilization (OIS), Electronic Image Stabilization (EIS), 4K/UHD Video Recording, Night HDR, and can capture images with resolution up to 48 MP.</p>
+		<br>
+		<h4>Magnificent Selfie Camera</h4>
+		<p>Make your selfies stand out from the crowd with the 8 MP front camera. It comes with a f/2.0 aperture, 83-degree field of view, and also supports Electronic Image Stabilization (EIS)</p>
+		<br>
+		<h4>Adaptive Display</h4>
+		<p>Enjoy a more visually engaging experience on this 15.7-cm (6.2) Full HD+ All-screen Adaptive display. With a 90% Screen-to-body ratio and 100% DCI-P3 coverage, you will be truly immersed whether you are watching videos or playing games.</p>
+		<br>
+		<h4>Intelligent Fast Charging</h4>
+		<p>Play games and watch videos for a longer period, thanks to the 3300-mAh high-capacity battery. It offers up to 13.5 hours of HD video playback, 25.5 hours of 3G talk time, and 14 hours of Wi-Fi browsing. With QuickCharge 3.0, you don't need to wait long to continue using your phone again.</p>
+		<br>
+		<h4>Flagship Audio</h4>
+		<p>Featuring 5-magnetic speakers and Dual-smart AMP, this smartphone takes your music-listening experience to the next level.</p>
+		<br>
+		<h4>Blazing-fast Connectivity</h4>
+		<p>With Bluetooth 5.0 and Apt:X HD Audio, you can easily connect to multiple speakers and devices. It also allows two 4G VoLTE connections for your convenience.</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col">
+		<h3>Buy the product in <a href="https://www.amazon.in/Renewed-Asus-Zenfone-ZS621KL-2A012IN-Midnight/dp/B07KYNW75C/ref=sr_1_1?dchild=1&keywords=asus+zenfone+5Z&qid=1586152071&sr=8-1" target="_blank"><img src="amazon.jpeg" style="width: 50px; height: 20px;"></a></h3>
+			<h3><a href="https://www.flipkart.com/asus-zenfone-5z-midnight-blue-64-gb/p/itmf5g475gajum8r?pid=MOBF5G47PUPQ8YDW&lid=LSTMOBF5G47PUPQ8YDWVQ8PLV&marketplace=FLIPKART&srno=s_1_3&otracker=search&otracker1=search&fm=SEARCH&iid=7dbc9862-2d97-4983-9f57-b75b1850f6d4.MOBF5G47PUPQ8YDW.SEARCH&ppt=sp&ppn=sp&ssid=utrxr56w7k0000001586151163517&qH=a1b317779aecbf41" target="_blank"><img src="flipkart.png" style="width: 30px; height: 30px;"></a></h3>
+	</div>
+</div>
+</div>
+<div class=" footer text-center">
+  <p>follow us on <a href="#" class="fa fa-facebook footer"></a> <a href="#" class="fa fa-instagram footer "></a> <a href="#" class="fa fa-twitter footer "></a> <a href="#" class="fa fa-telegram footer"></a></p>
+
+</div>
+</body>
+</html>
